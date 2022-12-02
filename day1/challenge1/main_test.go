3e17daa9699dc2fb92669a99668dd77c1e5986c1
Harold Arriola Nucamendi <hnucamend@hnucamendi.attlocal.net>
2022-12-02 07:45:39 -0500

    disaster

--- day1/challenge1/main_test.go
+++ day1/challenge1/main_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func Test_CalorieCounter(t *testing.T) {
-
 	type args struct {
 		calories []int
 	}
@@ -38,5 +37,103 @@ func Test_CalorieCounter(t *testing.T) {
 				t.Errorf("got %v, wanted %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Test_ElfParser(t *testing.T) {
+
+	type args struct {
+		in string
+	}
+
+	tests := []struct {
+		name    string
+		wantErr bool
+		args    args
+		want    []int
+	}{
+		{
+			name:    "Working Case",
+			wantErr: false,
+			args: args{
+				in: `
+					123
+					456
+					`,
+			},
+			want: []int{123, 456},
+		},
+		{
+			name:    "If Negative",
+			wantErr: false,
+			args: args{
+				in: `
+				-2132 
+				-11441
+					`,
+			},
+			want: []int{-2132, -11441},
+		},
+		{
+			name:    "if single line",
+			wantErr: false,
+			args: args{
+				in: "12414 343241",
+			},
+			want: []int{12414, 343241},
+		},
+	}
+
+	for _, tt := range tests {
+		got, err := ElfParser(tt.args.in)
+		if (err != nil) != tt.wantErr {
+			t.Fatalf("error %v expected want error %v", err, tt.wantErr)
+		}
+
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("got %v wanted %v", got, tt.want)
+		}
+	}
+}
+
+func TestMain(t *testing.T) {
+
+	type args struct {
+		cal []int
+		in  string
+	}
+
+	tests := []struct {
+		name    string
+		wantErr bool
+		args    args
+		want    int
+	}{
+		{
+			name:    "initial test",
+			wantErr: false,
+			args: args{
+				in:  "30123 32932",
+				cal: []int{30123, 32932},
+			},
+			want: 63055,
+		},
+	}
+
+	for _, tt := range tests {
+		gotString, err := ElfParser(tt.args.in)
+		if (err != nil) != tt.wantErr {
+			t.Fatalf("error %v want error %v", err, tt.wantErr)
+		}
+
+		got, err := CalorieCounter(gotString)
+		if (err != nil) != tt.wantErr {
+			t.Fatalf("error %v want error %v", err, tt.wantErr)
+		}
+
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("got %v want %v", got, tt.want)
+		}
+
 	}
 }
---
+++ day1/challenge1/test.js
@@ -0,0 +1,7 @@
+arr = [1, 3, 34, 45, 5];
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+console.log(arr);
